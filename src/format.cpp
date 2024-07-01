5f85452627b06b87a121056f1d68949711b98196
Mateusz Schoen <76482518+shonun1@users.noreply.github.com>
2024-07-01 12:34:04 +0900

    Time formatting

--- src/format.cpp
+++ src/format.cpp
@@ -1,6 +1,9 @@
+#include "format.h"
+
 #include <string>
 
-#include "format.h"
+#define HOUR 3600
+#define MINUTE 60
 
 using std::string;
 
@@ -8,4 +11,14 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  int hours = seconds / HOUR;
+  int minutes = (seconds % HOUR) / MINUTE;
+  int remaining_seconds = seconds % MINUTE;
+  string minutes_prefix = minutes < 10 ? "0" : "";
+  string seconds_prefix = remaining_seconds < 10 ? "0" : "";
+
+  return std::to_string(hours) + ":" + minutes_prefix +
+         std::to_string(minutes) + ":" + seconds_prefix +
+         std::to_string(remaining_seconds);
+}
