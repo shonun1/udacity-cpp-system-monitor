e30401eb2e36b6ff71ab6d8f480b5d878f734bce
Mateusz Schoen <76482518+shonun1@users.noreply.github.com>
2024-07-01 12:35:02 +0900

    Processor class

--- include/processor.h
+++ include/processor.h
@@ -1,12 +1,15 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
+#include <vector>
+
 class Processor {
  public:
   float Utilization();  // TODO: See src/processor.cpp
 
   // TODO: Declare any necessary private members
  private:
+  std::vector<long> _prev_cpu_util{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 };
 
 #endif
--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,38 @@
 #include "processor.h"
 
+#include <string>
+
+#include "linux_parser.h"
+
+using LinuxParser::CPUStates;
+
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() {
+  std::vector<long> cpu_values;
+  for (auto el : LinuxParser::CpuUtilization()) {
+    cpu_values.push_back(std::stol(el));
+  }
+
+  // algorithm based on
+  // https://stackoverflow.com/questions/23367857/accurate-calculation-of-cpu-usage-given-in-percentage-in-linux
+  long prev_idle =
+      _prev_cpu_util[CPUStates::kIdle_] + _prev_cpu_util[CPUStates::kIOwait_];
+  long idle = LinuxParser::IdleJiffies();
+
+  long prev_non_idle =
+      _prev_cpu_util[CPUStates::kUser_] + _prev_cpu_util[CPUStates::kNice_] +
+      _prev_cpu_util[CPUStates::kSystem_] + _prev_cpu_util[CPUStates::kIRQ_] +
+      _prev_cpu_util[CPUStates::kSoftIRQ_] + _prev_cpu_util[CPUStates::kSteal_];
+  long non_idle = LinuxParser::ActiveJiffies();
+
+  long prev_total = prev_idle + prev_non_idle;
+  long total = idle + non_idle;
+  long idled = idle - prev_idle;
+  long totald = total - prev_total;
+  long result_num = totald - idled;
+  float result = float(result_num) / float(totald);
+
+  _prev_cpu_util = cpu_values;
+
+  return result;
+}
